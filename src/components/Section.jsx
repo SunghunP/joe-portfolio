3064e364126420adbb00534911cad7fe987aa25e
Joe Park <sunghun.josephp@gmail.com>
2026-09-01 23:34:17 -0700

    Create Section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+import Shell from './Shell'
+
+export default function Section({ id, children, className = '' }) {
+    return (
+        <section id={id} className={`border-t border-border py-14 sm:py-20 ${className}`}>
+            <Shell>{children}</Shell>
+        </section>
+    )
+}
