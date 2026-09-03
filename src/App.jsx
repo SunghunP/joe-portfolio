956c889cf96b81f481ccfec61ec86c4001e14739
Joe Park <sunghun.josephp@gmail.com>
2026-09-03 12:28:27 -0700

    Scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import CaseStudy from './pages/CaseStudy';
 
 function App() {
   return (
     <BrowserRouter basename="/joe-portfolio">
+      <ScrollToTop />
       <div id="top">
         <Nav />
         <Routes>
