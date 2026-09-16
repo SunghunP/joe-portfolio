9913ce1c5cdadf6520ffb45ca5e5dd338bd0921a
Joe Park <sunghun.josephp@gmail.com>
2026-09-16 10:16:28 -0700

    Increase website shell width

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,6 +1,6 @@
 export default function Shell({ children, className = '' }) {
   return (
-    <div className={`mx-auto max-w-[1120px] px-5 sm:px-8 lg:px-11 ${className}`}>
+    <div className={`mx-auto max-w-[1240px] px-5 sm:px-8 lg:px-11 ${className}`}>
       {children}
     </div>
   )
